d04061b5cc1e5d65e0505b017f5fcc77a53187fb
Shubham <bhamshu@github.com>
2023-05-10 22:13:58 +0530

    Fix playercontr. Check Working checkpoint

--- app/controllers/api/v1/player_controller.rb
+++ app/controllers/api/v1/player_controller.rb
@@ -7,8 +7,11 @@ class Api::V1::PlayerController < ApplicationController
             render json: { error: 'Missing param uniq_pub_name' }, status: 400
             return
         end
-        
-        uniq_backend_id = SecureRandom.uuid
+
+        game = Game.new(state: INIT_STATE)
+        # Saving here so that there's as little gap between the (ideally)
+        # atomic operations of checking player unique pub name and saving it 
+        game.save
 
         # TODO: this should be atomic - checking for name and then registering
         unless Player.where(uniq_pub_name: uniq_pub_name).empty?
@@ -16,10 +19,7 @@ class Api::V1::PlayerController < ApplicationController
             return
         end
 
-        game = Game.new(game_state: INIT_STATE, game_stage: "STRATEGISE")
-        game.save
-
-        player = Player.new(uniq_pub_name: uniq_pub_name, uniq_backend_id: uniq_backend_id, game_id: game.id)
+        player = Player.new(uniq_pub_name: uniq_pub_name, games_id: game.id)
 
         unless player.save
             # This shouldn't happen - only case it could happen is simultaneously two
@@ -28,6 +28,6 @@ class Api::V1::PlayerController < ApplicationController
             return
         end
 
-        render json: {uniq_pub_name: uniq_pub_name, uniq_backend_id: uniq_backend_id }
+        render json: {uniq_pub_name: uniq_pub_name, uniq_backend_id: player.id }
     end
 end
